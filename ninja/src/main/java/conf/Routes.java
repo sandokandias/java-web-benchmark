ede1c03d8b8ab8b4036f9c223d40f3e886261f30
Thiago Amarante <thiagoaag@gmail.com>
2017-03-19 16:46:07 -0300

    Ninja framework

---
+++ ninja/src/main/java/conf/Routes.java
@@ -0,0 +1,33 @@
+
+package conf;
+
+
+import controllers.ApplicationController;
+import ninja.AssetsController;
+import ninja.Router;
+import ninja.application.ApplicationRoutes;
+
+public class Routes implements ApplicationRoutes {
+
+    @Override
+    public void init(Router router) {
+
+        //router.GET().route("/").with(ApplicationController::index);
+        router.GET().route("/hello_world.json").with(ApplicationController::helloWorldJson);
+
+
+        ///////////////////////////////////////////////////////////////////////
+        // Assets (pictures / javascript)
+        ///////////////////////////////////////////////////////////////////////    
+        router.GET().route("/assets/webjars/{fileName: .*}").with(AssetsController::serveWebJars);
+        router.GET().route("/assets/{fileName: .*}").with(AssetsController::serveStatic);
+
+        ///////////////////////////////////////////////////////////////////////
+        // Index / Catchall shows index page
+        ///////////////////////////////////////////////////////////////////////
+        router.GET().route("/.*").with(ApplicationController::index);
+
+        router.POST().route("/").with(ApplicationController::create);
+    }
+
+}
