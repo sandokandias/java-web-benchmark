45db75ce588fd403d9df09bf25104b9c1acbaabe
Sandokan Dias <sandokandias@gmail.com>
2017-03-19 16:00:43 -0300

    creating lab for spring-boot-jetty

---
+++ spring-boot-jetty/src/main/java/com/github/sandokandias/SpringBootJettyApplication.java
@@ -0,0 +1,12 @@
+package com.github.sandokandias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJettyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJettyApplication.class, args);
+	}
+}
