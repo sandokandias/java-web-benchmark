d0d4586911af5d8297c3f386d32a6cadb9e1fd89
Thiago Amarante <thiagoaag@gmail.com>
2017-03-19 21:12:14 -0300

    Ninja framework with database

--- ninja/src/main/java/model/TransactionItem.java
+++ ninja/src/main/java/model/TransactionItem.java
@@ -3,11 +3,21 @@ package model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 
 @Getter
 @Setter
+@Entity(name = "TransactionItem")
 public class TransactionItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String transactionId;
     private String code;
     private String name;
     private BigDecimal price;
